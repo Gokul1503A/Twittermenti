1c511955617f7af926c80726df774a523e2a4925
KOPPOLA GOKUL SAI <koppolagokulsai@KOPPOLAs-MacBook-Air.local>
2024-01-02 19:43:53 +0530

    final twiiterMneti

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    
+    
+        let senti = try? SentimentClassifier_1(configuration: MLModelConfiguration())
+
+    
+   
+    
+    
+   
+       
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+       
+       
     }
 
     @IBAction func predictPressed(_ sender: Any) {
+        
+        
+        if let prediction = try? senti?.prediction(text: textField.text!){
+            print(prediction.label)
+            
+            if prediction.label == "Pos"{
+                sentimentLabel.text = "😁"
+                print(prediction.label)
+            }
+            if prediction.label == "Neg"{
+                sentimentLabel.text = "☹️"
+                print(prediction.label)
+            }
+            if prediction.label == "Neutral"{
+                sentimentLabel.text = "😐"
+                print(prediction.label)
+            }
+        }
     
     
     }
